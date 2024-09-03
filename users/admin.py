a01218c2552df2cb97a52822ec8e2e6e581f19ad
wdh70743 <wdh70743@gmail.com>
2024-09-03 12:33:29 +1000

    Fixed admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'password', 'created_at')
+
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
