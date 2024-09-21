03265b4dd643eedd3ba7bb4b9c8d40aeed78febe
wdh70743 <wdh70743@gmail.com>
2024-09-21 22:51:31 +1000

    Added TASK models.py and edited project-level settings.py and urls.py

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
